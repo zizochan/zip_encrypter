3e1b01c115bdbd8c5b3947f3ecdcf16306d76978
zizo <zizo.toiawase@gmail.com>
2015-03-04 01:47:29 +0900

    拡張子判定

--- decrypt.sh
+++ decrypt.sh
@@ -4,10 +4,21 @@
 script_dir=$(cd $(dirname $0);pwd)
 . $script_dir/common_process.sh
 
+# check ext
+case $filename in
+*[^\.zip])
+        echo "[ERROR] not zip file can not be unzip"
+        exit
+esac
+
 # unzip
 cd $filepath && unzip -qeP $password $filename
 
 # finish
 echo "$target has been zip decryption"
+
+# show files (for mac)
+open $filepath
+
 exit
 
--- encrypt.sh
+++ encrypt.sh
@@ -4,6 +4,13 @@
 script_dir=$(cd $(dirname $0);pwd)
 . $script_dir/common_process.sh
 
+# check ext
+case $filename in
+*\.zip)
+	echo "[ERROR] zip file can not be zip"
+	exit
+esac
+
 # zip
 cd $filepath && zip -qeP $password $basename.zip $filename;
 
